1fa650308d60bbcf81fbeef62611f90d385d07f8
fsampson30@ <fsampson30@gmail.com>
2021-08-31 07:15:16 -0300

    HomeFragment displays information of total employees registered, total careers and last sync.

--- app/src/main/java/com/sampson/terceirokotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sampson/terceirokotlin/ui/home/HomeFragment.kt
@@ -1,31 +1,84 @@
 package com.sampson.terceirokotlin.ui.home
 
+import Model.Employee
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import com.google.gson.GsonBuilder
+import com.sampson.terceirokotlin.ApiService
 import com.sampson.terceirokotlin.R
+import kotlinx.android.synthetic.main.fragment_slideshow.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.*
+
+private const val BASE_URL = "https://ruy-manager-employee.herokuapp.com/"
+private const val TAG = "HomeFragment"
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-       homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        /*val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
-        return root
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val txtTotalEmployees = view.findViewById<TextView>(R.id.txtHomeFregmentQtdEmployees)
+        val txtTotalCareers = view.findViewById<TextView>(R.id.txtHomeFragmentQtdCareers)
+        val txtLastSync = view.findViewById<TextView>(R.id.txtHomeFragmentLastSync)
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBarHomeFregment)
+        progressBar.visibility = View.VISIBLE
+
+        val simpleDate = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate = simpleDate.format(Date())
+
+        val gson = GsonBuilder().create()
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+            GsonConverterFactory.create(gson)
+        ).build()
+        val apiService = retrofit.create(ApiService::class.java)
+        apiService.getAllEmployees().enqueue(object : Callback<List<Employee>> {
+            override fun onResponse(
+                call: Call<List<Employee>>,
+                response: Response<List<Employee>>
+            ) {
+                Log.i(TAG, "onResponse $response")
+                val list = response.body()
+                val listCareers = list!!.groupBy { it.jobTitle }
+                txtTotalEmployees.text = list.size.toString()
+                txtTotalCareers.text = listCareers.size.toString()
+                txtLastSync.text = "${getString(R.string.last_sync)} $currentDate"
+
+                Log.d(TAG, listCareers.toString())
+                if (response.body() == null) {
+                    Log.i(TAG, "Did not receive a valid response body")
+                    return
+                }
+                progressBar.visibility = View.GONE
+            }
+
+            override fun onFailure(call: Call<List<Employee>>, t: Throwable) {
+                Log.i(TAG, "onFailure $t")
+                Toast.makeText(context, "Timeout", Toast.LENGTH_SHORT).show()
+                pbCircularBar.visibility = View.GONE
+            }
+        })
+
+
+
+        return view
     }
 }
